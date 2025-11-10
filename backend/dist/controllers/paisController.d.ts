eea2f98c4441273fa2675777196c8b23ddf67947
Narciso Perez <narcisoperez@gmail.com>
2025-11-10 20:05:49 -0300

    Delete backend/dist directory

--- backend/dist/controllers/paisController.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-export declare const getPaises: (_req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-export declare const getPaisById: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-export declare const createPais: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-export declare const updatePais: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-export declare const deletePais: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-//# sourceMappingURL=paisController.d.ts.map
